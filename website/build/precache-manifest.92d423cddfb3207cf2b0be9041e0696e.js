1c56da3046e78713af0861e5d1ecc622ac39dcce
Henry He <hhe@soundhound.com>
2020-03-29 14:41:59 -0700

    build

---
+++ website/build/precache-manifest.92d423cddfb3207cf2b0be9041e0696e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd5fa737128442e137c806cf23e065df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f1369e709ee046a6bb4",
+    "url": "/static/css/2.5ad941cb.chunk.css"
+  },
+  {
+    "revision": "456af755093c12a1ae2c",
+    "url": "/static/css/main.28cdaa0e.chunk.css"
+  },
+  {
+    "revision": "5f1369e709ee046a6bb4",
+    "url": "/static/js/2.72107f4f.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.72107f4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456af755093c12a1ae2c",
+    "url": "/static/js/main.2a0e303e.chunk.js"
+  },
+  {
+    "revision": "85f41a3108a9b6db6832",
+    "url": "/static/js/runtime-main.d82219d9.js"
+  }
+]);
